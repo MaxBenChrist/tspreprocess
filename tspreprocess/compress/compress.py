dbefed939813fbfe7e368532e60f7a7480ab5cef
Maximilian Christ <max.christ@me.com>
2017-07-20 17:36:11 +0200

    we develop tspreprocess not tsfresh

--- tspreprocess/compress/compress.py
+++ tspreprocess/compress/compress.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# This file as well as the whole tsfresh package are licenced under the MIT licence (see the LICENCE.txt)
+# This file as well as the whole tspreprocess package are licenced under the MIT licence (see the LICENCE.txt)
 # Maximilian Christ (maximilianchrist.com), 2017
 
 from __future__ import absolute_import, division
